dd4e25a478b60c8b48152b641ff6ff84b74403c2
Kenerry Serain <kserain.development@gmail.com>
2024-05-09 18:52:24 -0300

    [Live] Version

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          EKS Live on Youtube
+          EKS Live on Youtube V1.0
         </p>
         <a
           className="App-link"
